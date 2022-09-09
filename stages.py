6e51b147a8b466455d3beb18a1adb8b1c1645704
Carlos Meisel <carlosmeisel2216@gmail.com>
2022-09-08 22:08:34 -0500

    correction

--- stages.py
+++ stages.py
@@ -22,7 +22,7 @@ def stage02i(pi_0c, p01, s01, h01):
     GP = GasProp()
     h02i = GP.h(s=s2prime, p=1)
     w_ci = h02i - h01
-    return h02i, w_ci, p02i
+    return h02i, w_ci, p02i, s02i
 
 def stage02(p02i, w_ci, h01, eff_c):
     GP = GasProp()
@@ -32,7 +32,7 @@ def stage02(p02i, w_ci, h01, eff_c):
     h02 = h01 + w_c
     s02_1bar = GP.s(h=h02, p=1)
     s02 = s02_1bar - 0.28716 * np.log(p02)
-    return s02, w_c, h02
+    return s02, w_c, h02, p02
 
 def stage03( TIT, h02, eff_comb, p03):
     minL = 14.66
@@ -81,16 +81,9 @@ def stage04(p04, w_t, h03, r, q):
     s04 = s04_1bar_mix - 0.28716 * np.log(p04)
     return s04, T04, h04
 
-def stage045i(BSFC,SHP, m_air, excess_air, eff_free_turbine, h04, r, q, s045i):
+def stage045i(SHP, m_air, excess_air, eff_free_turbine, h04, r, q, s045i):
     minL = 14.66
     w_PT = SHP/(m_air*(1+(1/(minL*excess_air))))
-    m_g = SHP/w_PT
-    m_fuel = BSFC*SHP *(1/3600)
-    L = minL*excess_air
-    m_air_real = m_fuel*L
-    # print('m_fuel = ', m_fuel)
-    # print('m_air_real = ', m_air_real)
-
     w_PTi = w_PT/eff_free_turbine
     h045i = h04 - w_PTi
     T045i = Iterate_temp_h(h045i, r, q)
@@ -101,9 +94,7 @@ def stage045i(BSFC,SHP, m_air, excess_air, eff_free_turbine, h04, r, q, s045i):
     s045i_1bar_stoich = GP.s(T=T045i, p=1)
     s045i_1bar_mix = r*s045i_1bar_stoich + q*s045i_1bar_air
     p045i = exp(-(s045i - s045i_1bar_mix)/0.28716)
-
-
-    return w_PT, T045i, h045i, p045i, m_fuel, m_air_real, m_g
+    return w_PT, T045i, h045i, p045i
 
 def stage045(p045, w_PT, h04, r, q):
     h045 = h04 - w_PT
@@ -115,7 +106,6 @@ def stage045(p045, w_PT, h04, r, q):
     s045_1bar_stoich = GP.s(T=T045, p=1)
     s045_1bar_mix = r*s045_1bar_stoich + q*s045_1bar_air
     s045 = s045_1bar_mix - 0.28716 * np.log(p045)
-<<<<<<< HEAD
     return h045, s045, T045
 
 def stage5ideal(r, q, s05i, p5i, h045, eff_nozzle, m_air, excess_air, eff_propeller, SHP):
@@ -126,8 +116,8 @@ def stage5ideal(r, q, s05i, p5i, h045, eff_nozzle, m_air, excess_air, eff_propel
     T5i_air = GP.T(s=s05i_1bar, p=1)
     GP.combustion(lamb=1)
 
-    T5i_stoich = GP.T(s=s05i_1bar, p=1)
-    # T5i_stoich = 687.16 # HARDCODED VALUE FIX THIS LATER
+    # T5i_stoich = GP.T(s=s05i_1bar, p=1)
+    T5i_stoich = 718.16 # HARDCODED VALUE FIX THIS LATER
     T5i = r*T5i_stoich + q*T5i_air
 
     # T5i = Iterate_temp_ps(s05i_1bar, r, q)
@@ -138,12 +128,10 @@ def stage5ideal(r, q, s05i, p5i, h045, eff_nozzle, m_air, excess_air, eff_propel
 
     c5i = sqrt(2*((h045*1000) - (h5i*1000)))
     c5 = c5i * eff_nozzle
-    Thrust = m_air*c5*(1+(1/(minL*excess_air)))
+    Spec_Thrust = m_air*c5*(1+(1/(minL*excess_air)))
+    m_fuel = m_air*(1/(minL*excess_air))
 
-    return T5i, h5i, c5, Thrust
-=======
-    return h045, s045
->>>>>>> parent of d7f9236 (Full cycle)
+    return T5i, h5i, c5, Spec_Thrust, m_fuel
 
 def stage5i(r, q, s05i, p5i, h045, eff_nozzle, m_air, excess_air, eff_propeller, SHP):
     s05i_1bar = s05i + 0.28716 * np.log(p5i)
@@ -152,23 +140,16 @@ def stage5i(r, q, s05i, p5i, h045, eff_nozzle, m_air, excess_air, eff_propeller,
     GP.air()
     T5i_air = GP.T(s=s05i_1bar, p=1)
     GP.combustion(lamb=1)
-    # print(s05i_1bar)
 
-<<<<<<< HEAD
-    # T5i_stoich = GP.T(s=s05i_1bar, p=1)
-    T5i_stoich = 738.6 # HARDCODED VALUE FIX THIS LATER
-=======
-    # T5i_stoich = GP.T(p=1, s=s05i_1bar)
-    T5i_stoich = 711.6 # HARDCODED VALUE FIX THIS LATER
->>>>>>> parent of d7f9236 (Full cycle)
+    T5i_stoich = GP.T(s=s05i_1bar, p=1)
+    # T5i_stoich = 718.16 # HARDCODED VALUE FIX THIS LATER
     T5i = r*T5i_stoich + q*T5i_air
 
-    T5i = Iterate_temp_ps(s05i_1bar, r, q)
+    # T5i = Iterate_temp_ps(s05i_1bar, r, q)
     h5i_stoich = GP.h(T=T5i)
     GP.air()
     h5i_air = GP.h(T=T5i)
     h5i = r*h5i_stoich + q*h5i_air
-    print('h5i', h5i)
 
     c5i = sqrt(2*((h045*1000) - (h5i*1000)))
     c5 = c5i * eff_nozzle
