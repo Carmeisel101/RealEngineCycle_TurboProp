f70056eb2b671e12117e38177a26f63821a0a0ff
Carlos Meisel <carlosmei33@outlook.com>
2022-11-20 23:06:34 -0600

    step4/12

--- task6/Steps.py
+++ task6/Steps.py
@@ -1,6 +1,14 @@
-
+import numpy as np
+from sympy import Eq, solve, var
 
 def step1(N_n, w_c_n):
+    '''
+
+    :param N_n: Nominal angular speed
+    :param w_c_n: Nominal compressor work
+    :return w_c: compressor work
+    :return N: angular speed
+    '''
 
     N_r = N_n /1.05
     N = N_r * 1.1
@@ -9,6 +17,17 @@ def step1(N_n, w_c_n):
     return w_c, N
 
 def step2(N, gamma, N_n, pi_c, eff_com):
+    '''
+
+    :param N: Angular speed
+    :param gamma: ratio of specific heats
+    :param N_n: Nominal angular speed
+    :param pi_c: pressure ratio
+    :param eff_com: combustion efficiency
+    :return pi_c_star1: pressure ratio
+    :return eff_com_list: list of compressor efficiencies
+    :return pi_c_star_list: list of pressure ratios
+    '''
 
     tolerance = 0.01
     eff_com_list = []
@@ -33,5 +52,30 @@ def step2(N, gamma, N_n, pi_c, eff_com):
 
     return pi_c_star1, eff_com_list, pi_c_star_list
 
-def step3()
+def step3(eff_com, minL, lamb, m_air, T_01, p_03, TIT, p_01, pi_c_star):
+    '''
+
+    :param eff_com: combustion efficiency
+    :param minL: minL
+    :param lamb: excess air
+    :param m_air: mass of air
+    :param T_01: T_01
+    :param p_03: p_03
+    :param TIT: Turbine inlet temperature
+    :param p_01: p_01
+    :param pi_c_star: pressure ratio
+    :return sol : T_03
+    '''
+
+    const = ((1+(1/(lamb*minL)))/eff_com)*((p_03)/(((m_air)*(1+(1/(lamb*minL))))*np.sqrt(TIT)))
+    mass_const = m_air * (np.sqrt(T_01) / p_01)
+
+    T = var('T')
+    pi_c = const*mass_const*(((T/T_01)**(1/2)))
+    eq = Eq(pi_c, pi_c_star)
+    sol = solve(eq, T)
+    sol = float(sol[0])
+
+    return sol
+
 
--- task6/task6main.py
+++ task6/task6main.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
 
     specs_df = pd.read_csv('../task1/BasicSpecs.csv')
     task1_r2_df = pd.read_csv('../task1/results2.csv')
+    task1_r1_df = pd.read_csv('../task1/results.csv')
     task2_df = pd.read_csv('../task2/comp_stages.csv')
 
 
@@ -16,14 +17,27 @@ if __name__ == '__main__':
     pi_c = specs_df['Compressor Ratio'][0]
     eff_com = specs_df['eff_compressor'][0]
     N_n = task2_df['n'][0]
+    m_air = task1_r2_df['m_air'][0]
     T_01 = 288.16 # K
+    lamb = task1_r2_df['lambda'][0]
     gamma = 1.4
+    minL = 14.66
+    eff_combust = 1
+    p_03 = task1_r1_df['Pressure [bar]'][5]
+    p_01 = task1_r1_df['Pressure [bar]'][1]
+    cp = 1.004 # kJ/kgK
 
 
     w_c, N = step1(N_n, w_c_n)
     print('w_c = ', w_c, 'kJ/kg')
 
     pi_c_star, eff_com_list, pi_c_star_list = step2(N, gamma, N_n, pi_c, eff_com)
+    print('pi_c_star = ', pi_c_star)
+
+    T_03 = step3(eff_com, minL, lamb, m_air, T_01, p_03, TIT, p_01, pi_c_star)
+    print('T_03 = ', T_03)
+
+    print('wc/h3 = ', w_c/(cp*T_03))
 
 
 
